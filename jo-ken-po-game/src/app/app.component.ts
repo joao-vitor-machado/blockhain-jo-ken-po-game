99adcaeed6342bc8037ab548c3ea2fa15d377a6f
joao-vitor-machado <jv.muca@gmail.com>
2023-04-24 12:57:19 -0300

    enabling metamask installation verification

--- jo-ken-po-game/src/app/app.component.ts
+++ jo-ken-po-game/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import detectEthereumProvider from '@metamask/detect-provider';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jo-ken-po-game';
+
+  // constructor(private window: Window) {}
+
+  public doesUserHaveWalletProvider() {
+    return (window as any).ethereum;
+  }
 }
